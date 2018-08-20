70672db6919b6e04b29afe58d8fd07f9b9fbc58c
myasinK <yasinul@gmail.com>
2018-08-20 13:00:58 -0700

    Create custom exception

---
+++ ExceptionHandling-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling_2
+{
+    public class AgeExceptions : Exception
+    {
+        public AgeExceptions() : base() { }
+        public AgeExceptions(string message) : base(message) { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string input = Console.ReadLine();
+            int yearBorn = DateTime.Now.Year;
+            const string errorMsg = "Invalid age - age must be greater than zero...and this program refuses to believe you're over 100";
+            try
+            {
+                int age = Convert.ToInt32(input);
+                if (age <= 0 || age >= 100)
+                {
+                    throw new AgeExceptions();
+                }
+            }
+            catch (AgeExceptions)
+            {
+                Console.WriteLine(errorMsg);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid age input");
+            }
+            
+                    
+        }
+    }
+}
